333c78be163e43cdb0cabd0ea269dd902a7767e4
Rahul Jha <jharahul1195@gmail.com>
2022-02-25 17:09:16 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn urishortner.wsgi:application --log-file -
---
+++ shortner/models.py
@@ -0,0 +1,12 @@
+from os import link
+from pyexpat import model
+import uuid
+from django.db import models
+
+
+# Create your models here.
+
+class Url(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
+
